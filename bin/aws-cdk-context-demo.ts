3f33f29afc9cdcec4c8d4099354add824490b6c9
lior-dux_clbt <lior.dux@cellebrite.com>
2025-03-23 18:12:06 +0200

    Stage 2 added.

--- bin/aws-cdk-context-demo.ts
+++ bin/aws-cdk-context-demo.ts
@@ -8,10 +8,19 @@ const app = new App();
 const env = app.node.tryGetContext("env");
 console.log(`env: ${env}`);
 
+// Consume environment context parameters and pass them to the stack
+const envContext = app.node.tryGetContext(env);
+console.log(`envContext: ${JSON.stringify(envContext)}`);
+
+const hello = envContext.hello;
+console.log(`hello: ${hello}`);
+
 new AwsCdkContextDemoStack(app, `${env}-AwsCdkContextDemoStack`, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
+  // Pass the context here
+  // envContext: envContext,
 });
 
